b91f1d8050729230018b73976117d435fcad3d03
Marko Bublic <bublic@afca.ch>
2016-04-10 15:06:17 +0200

    Initial Commit with Iris-DataSet

---
+++ Kohonen/Kohonen.WPF/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Kohonen.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application { }
+}
---
+++ Kohonen/Kohonen.WPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace Kohonen.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
